5b0a3f84adabd2f8a5cc8cffcea528e70ea547c3
Chris Davies <christophertdavies@gmail.com>
2024-07-05 12:29:36 +0100

    Version 2.0. Chgnet Support. Better handling for uninstalled packages.

--- sim_eval/calculators/__init__.py
+++ sim_eval/calculators/__init__.py
@@ -1,4 +1,34 @@
 # simulation_benchmarks/calculators/__init__.py
+
+import warnings
+import sys
+
+def warn_with_print(message, category, filename, lineno, file=None, line=None):
+    print(f'\033[93mWarning: {message}\033[0m', file=sys.stderr)
+
+warnings.showwarning = warn_with_print
+
+# Always import these
 from .base_calculator import PropertyCalculator
 from .vasp_xml_calculator import VASPXMLPropertyCalculator
-from .nequip_calculator import NequIPPropertyCalculator
+
+# Try to import NequIP-related classes
+try:
+    from .nequip_calculator import NequIPPropertyCalculator
+except ImportError as e:
+    message = f"NequIP is not installed. NequIPPropertyCalculator will not be available. To use it, please install NequIP using 'pip install nequip'. Error: {str(e)}"
+    warnings.warn(message, ImportWarning)
+    print(f'\033[93mWarning: {message}\033[0m', file=sys.stderr)
+    NequIPPropertyCalculator = None
+
+# Try to import CHGNet-related classes
+try:
+    from .chgnet_calculator import CHGNetPropertyCalculator
+except ImportError as e:
+    message = f"CHGNet is not installed. CHGNetPropertyCalculator will not be available. To use it, please install CHGNet using 'pip install chgnet'. Error: {str(e)}"
+    warnings.warn(message, ImportWarning)
+    print(f'\033[93mWarning: {message}\033[0m', file=sys.stderr)
+    CHGNetPropertyCalculator = None
+
+__all__ = ['PropertyCalculator', 'VASPXMLPropertyCalculator', 'NequIPPropertyCalculator', 'CHGNetPropertyCalculator']
+
